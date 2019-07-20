612fb94c4954e97dffca4fd53240032fc0031439
RSijelmass <R.Sijelmass@gmail.com>
2019-07-20 16:49:31 +0100

    Initialise

---
+++ app/controllers/clues_controller.rb
@@ -0,0 +1,4 @@
+class CluesController < ApplicationController
+  def index
+  end
+end
---
+++ test/controllers/clues_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CluesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get clues_index_url
+    assert_response :success
+  end
+
+end
